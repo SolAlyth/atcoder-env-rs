4a895e5226d8cb8fe74baf1f99468027df70a386
SolAlyth <sol.nkj.rall@gmail.com>
2023-08-27 23:27:55 +0900

    spacing

--- templates/template.rs
+++ templates/template.rs
@@ -45,7 +45,7 @@ mod mylib {
     use std::{fmt::Display, ops::Shl, cell::UnsafeCell};
     pub struct Solver { v: UnsafeCell<String>, b: bool }
     impl Solver {pub fn new(b: bool) -> Self {Solver{v:String::new().into(),b}} pub fn print(&self) {unsafe{let s=&mut*self.v.get();println!("{}", s);s.clear();}}}
-    impl<T: Display> Shl<T> for &Solver {type Output=Self;fn shl(self,rhs:T)->Self::Output{unsafe{let s=&mut*self.v.get();if s.len()!=0{s.push(' ');}s.push_str(&format!("{}",rhs));}self}}
+    impl<T: Display> Shl<T> for &Solver {type Output=Self; fn shl(self,rhs:T)->Self::Output{unsafe{let s=&mut*self.v.get();if s.len()!=0{s.push(' ');}s.push_str(&format!("{}",rhs));}self}}
     #[allow(non_camel_case_types)] pub struct end;
-    impl Shl<end> for &Solver {type Output=();fn shl(self,_:end)->Self::Output{if cfg!(debug_assertions)||self.b{self.print();}()}}
+    impl Shl<end> for &Solver {type Output=(); fn shl(self,_:end)->Self::Output{if cfg!(debug_assertions)||self.b{self.print();}()}}
 }
